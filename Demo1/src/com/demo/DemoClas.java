aab0dc8f8cf40d496933a53404211ba6850c50c8
salman <syed.sallu10@gmail.com>
2021-09-11 12:26:24 +0530

    project

---
+++ Demo1/src/com/demo/DemoClas.java
@@ -0,0 +1,25 @@
+
+	
+
+public class DemoClas {
+
+	public static void main(String[] args) {
+		
+		//In java Strings are immutable
+		// Immutable means unmodifiable or unchanable
+		
+		String username = "noah";
+		
+		//concat()--> updatin a string
+		
+		username.concat("hii");
+		
+		System.out.println(username);
+		
+		
+		
+		
+
+	}
+
+}
